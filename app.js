31c18743c90b95e5388c9658cfed6dc69cdc32c9
slddays <kimhnh@gmail.com>
2023-11-02 20:12:45 +0900

    fix: prevent tags from previous requests to show

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-/* TO DO LIST: Fix refresh quote*/
-
 document.addEventListener('DOMContentLoaded', () => {
   const author = document.getElementById('author');
-  const tagTwo = document.getElementById('tagTwo');
   const quote = document.getElementById('quote');
-
   const copyQuote = document.getElementById('copyQuote');
   const refreshQuote = document.getElementById('refreshQuote');
 
-  // Fetch a quote from API
+  // Fetch API
   async function randomQuote() {
     try {
       const res = await fetch('https://api.quotable.io/quotes/random');
       const data = await res.json();
-      console.log(data);
       author.textContent = data[0].author;
       quote.textContent = `\u201c${data[0].content}\u201d`;
 
-      // Dynamically add tags based on quote
+      // Empty tag div per API Request
+      const tagBlock = document.querySelector('.tag-block');
+      tagBlock.innerHTML = '';
+
+      // Dynamically add tags
       if (data[0].tags.length > 0) {
         for (let i = 0; i < data[0].tags.length; i++) {
           let tag = document.createElement('span');
           tag.textContent = data[0].tags[i];
-          document.querySelector('.tag-block').appendChild(tag);
+          tagBlock.appendChild(tag);
         }
       }
 
@@ -36,9 +35,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Refresh button --> Scuffed method. Not sure how to recall API and delete existing data.
+  // Refresh button
   refreshQuote.addEventListener('click', () => {
-    // location.reload();
     randomQuote();
   });
 
